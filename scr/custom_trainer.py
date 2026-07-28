4c97187945fc776237201ed555cccba4d92b6b40
NoorBayan <157401879+NoorBayan@users.noreply.github.com>
2026-07-28 23:42:07 +0300

    Create custom_trainer.py

---
+++ scr/custom_trainer.py
@@ -0,0 +1,17 @@
+import torch
+from transformers import Trainer
+import torch.nn as nn
+
+class WeightedTrainer(Trainer):
+    def __init__(self, class_weights, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.class_weights = torch.tensor(class_weights, dtype=torch.float32).to(self.args.device)
+
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
+        labels = inputs.pop("labels")
+        outputs = model(**inputs)
+        logits = outputs.logits
+        loss_fct = nn.CrossEntropyLoss(weight=self.class_weights)
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        
+        return (loss, outputs) if return_outputs else loss
